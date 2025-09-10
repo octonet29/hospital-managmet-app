75a8fd5521f9f4b6b4882177365cb4779364ca6a
Gulbahar <58729614+octonet29@users.noreply.github.com>
2025-09-10 15:00:53 +0500

    'first-commit'

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from "react-native"
+import "./global.css"
+
+export default function Index() {
+	return (
+		<SafeAreaView className="flex-1 items-center justify-center bg-white">
+			<Text className="text-xl font-bold text-blue-500">
+				Welcome to Nativewind!
+			</Text>
+		</SafeAreaView>
+	)
+}
